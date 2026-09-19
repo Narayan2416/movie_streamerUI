c36d9a69d0f16ba926ed60b6834164bdc4d3e6ba
Narayan2416 <phnarayanamoorthy@gmail.com>
2026-09-19 18:09:58 +0530

    added x-matroska version

--- js/movie.js
+++ js/movie.js
@@ -1,6 +1,8 @@
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
+const source = document.createElement("source");
 const video = document.getElementById("videoPlayer");
+source.type = "video/x-matroska";
 
 video.src = `${API_URL}/api/movies/play/${id}`;
